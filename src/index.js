386a89abbb627831bc2126e73ccd09f0836a7c5a
danyalshah3 <72283767+danyalshah3@users.noreply.github.com>
2021-08-13 10:24:51 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 console.log('%c HI', 'color: firebrick')
+
+
+
+
+
+function loadImages() {
+    const imgContainer = document.getElementById("dog-image-container").addEventListener("click", loadImages)
+    const img = document.createElement("img")
+    imgContainer.append(img)
+    fetch("https://dog.ceo/api/breeds/image/random/4")
+    .then(resp => resp.json())
+    .then(response => {
+        const dog = responce.message
+        document.querySelector("img").innerHTML += `<div><img src=${dog} alt="A Dog"/></div>`
+    })
+}
+
+
+
