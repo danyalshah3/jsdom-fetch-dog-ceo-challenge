0879d9f45b9cdbe5d62f41947afff14016848a84
danyalshah3 <72283767+danyalshah3@users.noreply.github.com>
2021-08-13 12:09:44 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 console.log('%c HI', 'color: firebrick')
 
-document.addEventListener("DOMContentLoaded", loadImages)
+document.addEventListener("DOMContentLoaded", () => {
+    loadImages()
+  });
 
 
 function renderImage(dog) {
     
     const img = document.createElement("img")
     img.src = dog
-    const imageContainer = document.getElementById("dog-image-container").addEventListener("click", loadImages)
+    const imageContainer = document.getElementById("#dog-image-container").addEventListener("click", loadImages)
     imageContainer.append(img)
+}
+
+
+function renderAllImages(response) {
+    const images = response.message
+     images.forEach(dog => {
+    renderImage(dog)
+     })
+
 }
 
 
@@ -16,14 +27,10 @@ function renderImage(dog) {
 function loadImages() {
     fetch("https://dog.ceo/api/breeds/image/random/4")
     .then(resp => resp.json())
-    .then(images => {
-        console.log(images)
-        console.log(images.message)
-    //  images.messages.forEach(image =>
-    //     renderImage(image))
-    })
+    .then(renderAllImages)
+    
 }
 
 
-
+  
 
