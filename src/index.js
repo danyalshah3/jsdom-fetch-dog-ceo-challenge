f3e732267c7e89b093fc8595540b90bc29fe2044
danyalshah3 <72283767+danyalshah3@users.noreply.github.com>
2021-08-14 17:13:45 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
+let breedContainer = document.getElementById("dog-breeds")
 console.log('%c HI', 'color: firebrick')
 
+
+
 document.addEventListener("DOMContentLoaded", () => {
-    loadImages()
-  });
+    loadImages(), loadBreeds(), filterBreed()
+});
 
 
 function renderImage(dog) {
     
     const imageContainer = document.getElementById("dog-image-container")
     const img = document.createElement("img")
-     img.src = dog
+    img.src = dog
     // img.innerHTML = `<div><img src=${images} alt="A Dog"/></div>`
     imageContainer.append(img)
 }
@@ -17,10 +20,10 @@ function renderImage(dog) {
 
 function renderAllImages(array) {
     const images = array.message
-     images.forEach(dog => {
-    renderImage(dog)
-     })
-
+    images.forEach(dog => {
+        renderImage(dog)
+    })
+    
 }
 
 
@@ -34,18 +37,60 @@ function loadImages() {
 
 
 function renderBreed(breed) {
-     const breedList= document.getElementById("dog-breeds")
-     const li = document.createElement("li")
-     lis.textContent = breed
-     breedList.append(li)
-
+    
+    let li = document.createElement("li")
+    li.textContent = breed
+    breedContainer.append(li);
+    li.addEventListener("click", function(event) {
+        event.target.style.color = "blue";
+      });
+    
 }
 
   
+let breedList = []
 
 function loadBreeds() {
     fetch("https://dog.ceo/api/breeds/list/all")
-    .then(resp => resp.json)
-    .then()  
+    .then(resp => resp.json())
+    .then(list => {
+
+         breedList = Object.keys(list.message)
+        breedList.forEach(breed => {
+         renderBreed(breed)
+        })
+    })  
 }
 
+
+
+//  function changeColor (){
+// document.getElementById("dog-breeds").addEventListener("click", event => {
+//     // use a condition to figure out what element was clicked
+//     if (event.target.matches("li")) {
+//       // run our DOM logic
+//       event.target.style.color = "red"
+//     }
+//   })
+// }
+
+function filterBreed() {
+const dropDown = document.getElementById("breed-dropdown")
+dropDown.addEventListener("change", (e) => {
+    let letter = e.target.value
+    // console.log(letter, e.target.value)
+     let array = breedList.filter(dog => {
+       return dog.startsWith(letter)
+        
+    })
+    breedContainer.innerHTML = ""
+    array.forEach(breed => {
+        renderBreed(breed)
+    })
+})
+}
+
+  
+// dropDown.addEventListener("change", e() => {
+//     const letter = e.target.value
+// })
